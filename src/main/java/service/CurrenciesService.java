4ce90b646f7636fa171298ae9c3d9382ba92ca89
Spacier829 <mrsimpleeegor@gmail.com>
2025-01-22 17:28:40 +0700

    feat(main): implemented service for currencies

--- src/main/java/service/CurrenciesService.java
+++ src/main/java/service/CurrenciesService.java
@@ -1,9 +1,30 @@
+package service;
+
+import dao.CurrencyDaoImpl;
 import dto.CurrencyResponseDto;
+import entity.CurrencyEntity;
+import util.Mapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CurrenciesService {
-  public List<CurrencyResponseDto> getCurrencies() {
-    
+  private static final CurrenciesService INSTANCE = new CurrenciesService();
+
+  private final CurrencyDaoImpl currencyDao = CurrencyDaoImpl.getInstance();
+
+  private CurrenciesService() {
+  }
+
+  public static CurrenciesService getInstance() {
+    return INSTANCE;
+  }
+
+  public List<CurrencyResponseDto> findAll() {
+    return currencyDao.findAll().stream().map(Mapper::toResponseDto).collect(Collectors.toList());
+  }
+
+  public CurrencyResponseDto findByCode(String code) {
+    return currencyDao.findByCode(code).map(Mapper::toResponseDto).orElse(null);
   }
 }
