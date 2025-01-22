2bc30f0e3b96aed104f2ac2ab28cc26b8b3de983
Spacier829 <mrsimpleeegor@gmail.com>
2025-01-22 17:28:14 +0700

    feat(main): implemented get request for currencies

--- src/main/java/servlet/CurrencyServlet.java
+++ src/main/java/servlet/CurrencyServlet.java
@@ -1,2 +1,34 @@
-package servlet;public class CurrencyServlet {
+package servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dao.CurrencyDaoImpl;
+import dto.CurrencyResponseDto;
+import exception.DaoException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import service.CurrenciesService;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/currency/*")
+public class CurrencyServlet extends HttpServlet {
+  private final CurrenciesService currenciesService = CurrenciesService.getInstance();
+  private final CurrencyDaoImpl currencyDao = CurrencyDaoImpl.getInstance();
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    try {
+      List<CurrencyResponseDto> currencies = currenciesService.findAll();
+      resp.setContentType("application/json");
+      resp.setCharacterEncoding("UTF-8");
+      objectMapper.writeValue(resp.getWriter(), currencies);
+    } catch (DaoException exception) {
+      objectMapper.writeValue(resp.getWriter(), exception.getMessage());
+    }
+  }
 }
