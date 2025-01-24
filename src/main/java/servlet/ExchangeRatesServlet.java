f63e7f3944f37fb4a47a1802990a360be8671774
Spacier829 <mrsimpleeegor@gmail.com>
2025-01-24 15:18:08 +0700

    feat(main): added exchange rate servlet

--- src/main/java/servlet/ExchangeRatesServlet.java
+++ src/main/java/servlet/ExchangeRatesServlet.java
@@ -1,2 +1,26 @@
-package servlet;public class ExchangeRatesServlet {
+package servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dto.ExchangeRateResponseDto;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import service.ExchangeRatesService;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/exchangeRates")
+public class ExchangeRatesServlet extends HttpServlet {
+  private final ExchangeRatesService exchangeRatesService = ExchangeRatesService.getInstance();
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    resp.setContentType("application/json");
+    resp.setCharacterEncoding("UTF-8");
+    List<ExchangeRateResponseDto> exchangeRates = exchangeRatesService.findAll();
+    objectMapper.writeValue(resp.getWriter(), exchangeRates);
+  }
 }
