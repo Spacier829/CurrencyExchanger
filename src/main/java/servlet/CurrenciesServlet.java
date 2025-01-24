5ef960a92059ec0bfa05e3379b54b1e79789bc35
Spacier829 <mrsimpleeegor@gmail.com>
2025-01-24 16:46:36 +0700

    refactor(main): removed charset init

--- src/main/java/servlet/CurrenciesServlet.java
+++ src/main/java/servlet/CurrenciesServlet.java
@@ -19,8 +19,6 @@ public class CurrenciesServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    resp.setContentType("application/json");
-    resp.setCharacterEncoding("UTF-8");
     List<CurrencyResponseDto> currencies = currenciesService.findAll();
     objectMapper.writeValue(resp.getWriter(), currencies);
   }
