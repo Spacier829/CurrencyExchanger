ad992fe1c57b4b9204f1c074d6f4769328df9771
Spacier829 <mrsimpleeegor@gmail.com>
2025-01-24 15:14:14 +0700

    feat(main): added dto for exchange rates

---
+++ src/main/java/dto/ExchangeRateResponseDto.java
@@ -0,0 +1,53 @@
+package dto;
+
+import java.math.BigDecimal;
+
+public class ExchangeRateResponseDto {
+  private Long id;
+  private CurrencyResponseDto baseCurrency;
+  private CurrencyResponseDto targetCurrency;
+  private BigDecimal rate;
+
+  public ExchangeRateResponseDto() {
+  }
+
+  public ExchangeRateResponseDto(Long id, CurrencyResponseDto baseCurrency, CurrencyResponseDto targetCurrency,
+                                 BigDecimal rate) {
+    this.id = id;
+    this.baseCurrency = baseCurrency;
+    this.targetCurrency = targetCurrency;
+    this.rate = rate;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public CurrencyResponseDto getBaseCurrency() {
+    return baseCurrency;
+  }
+
+  public void setBaseCurrency(CurrencyResponseDto baseCurrency) {
+    this.baseCurrency = baseCurrency;
+  }
+
+  public CurrencyResponseDto getTargetCurrency() {
+    return targetCurrency;
+  }
+
+  public void setTargetCurrency(CurrencyResponseDto targetCurrency) {
+    this.targetCurrency = targetCurrency;
+  }
+
+  public BigDecimal getRate() {
+    return rate;
+  }
+
+  public void setRate(BigDecimal rate) {
+    this.rate = rate;
+  }
+}
