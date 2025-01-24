0ca76d609cfd1722118b731e136d2f0b12730dc7
Spacier829 <mrsimpleeegor@gmail.com>
2025-01-24 15:15:02 +0700

    feat(main): added convert exchangeRate entity -> DTO

--- src/main/java/util/Mapper.java
+++ src/main/java/util/Mapper.java
@@ -2,10 +2,12 @@ package util;
 
 import dto.CurrencyResponseDto;
 import dto.ErrorDto;
+import dto.ExchangeRateResponseDto;
 import entity.CurrencyEntity;
+import entity.ExchangeRateEntity;
 
 public class Mapper {
-  public static CurrencyResponseDto toResponseDto(CurrencyEntity currency) {
+  public static CurrencyResponseDto currencyToResponseDto(CurrencyEntity currency) {
     return new CurrencyResponseDto(
         currency.getId(),
         currency.getFullName(),
@@ -15,5 +17,13 @@ public class Mapper {
 
   public static ErrorDto errorDto(String message) {
     return new ErrorDto(message);
+  }
+
+  public static ExchangeRateResponseDto exchangeRateToResponseDto(ExchangeRateEntity exchangeRate) {
+    return new ExchangeRateResponseDto(
+        exchangeRate.getId(),
+        currencyToResponseDto(exchangeRate.getBaseCurrency()),
+        currencyToResponseDto(exchangeRate.getTargetCurrency()),
+        exchangeRate.getRate());
   }
 }
